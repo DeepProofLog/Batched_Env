614fd300bb0e67ed67e2ac6f2da23530bf47499a
JIAOYIING <jiaoying0329@gmail.com>
2024-10-20 11:41:56 +0200

    unification with prolog

--- janus/ancestor.pl
+++ janus/ancestor.pl
@@ -4,3 +4,5 @@ parent(charlie, mary).
 
 ancestor(X,Y) :- parent(X,Y).
 ancestor(X,Y) :- parent(X,Z), ancestor(Z,Y).
+
+proof_first([H | T], T) :- call(H).
--- janus/main.py
+++ janus/main.py
@@ -6,18 +6,15 @@ random.seed(23)
 
 
 janus.consult("ancestor.pl")
+# res = janus.query("proof_first([parent(charlie, _1), ancesor(_1, alice)], _T), term_string(_T, T)")
+# for d in res:
+#     print(d)
 
 state = deque(["ancestor(charlie, alice)"])
 
-def extract_var(text):
-    var = re.search(r'_(\d+)', text)
-    if var:
-        return var.group()
-    else:
-        return None
-
 while state:
-    print(state)
+    print("\n\n"+"*"*50)
+    print(f'current state is {", ".join(state)}')
     s = state.popleft()
     actions = []
     res = janus.query_once(f"clause({s}, _B), term_string(_B, B)")
@@ -25,36 +22,32 @@ while state:
         state = []
         reward = -1
         print("proof failed")
+        print(f'reward is {reward}')
     elif res["B"] == "true":
-        if not extract_var(s):
-            if not state:
-                reward = 1
-                print("proof succeeded")
+        if not state:
+            reward = 1
+            print("proof succeeded")
+            print(f'reward is {reward}')
         else:
-            var = extract_var(s)
-            s = s.replace(var, "REPLACE")
-            res = janus.query_once(f"clause({s}, _B), term_string(_B, B)")
-            if not res["truth"]:
-                state = []
-                reward = -1
-                print("proof failed")
-            else:
-                for d in janus.query(f"clause({s}, _B), term_string(_B, B)"):
-                    unification = d['REPLACE']
-                    actions.append(", ".join(state).replace(var, unification))
-                print(actions)
-                #agent = actions[0]
-                agent = random.choice(actions)
-                clauses = re.findall(r'\w+\(.*?\)', agent)
-                state = deque(clauses)
-                reward = 0
+            state_list = "["+ s + ", " + ", ".join(state)+"]"
+            res = janus.query(f"proof_first({str(state_list)}, _T), term_string(_T, T)")
+            for d in res:
+                actions.append(d["T"])
+            print(f' possible actions are {"; ".join(actions)}')
+            # agent = actions[1]
+            agent = random.choice(actions)
+            clauses = re.findall(r'\w+\(.*?\)', agent)
+            state = deque(clauses)
+            reward = 0
+            print(f'reward is {reward}')
     else:
         for d in janus.query(f"clause({s}, _B), term_string(_B, B)"):
             actions.append(d['B'])
-        print(actions)
+        print(f' possible actions are {"; ".join(actions)}')
         #agent = actions[1]
         agent = random.choice(actions)
         clauses = re.findall(r'\w+\(.*?\)', agent)
         for clause in reversed(clauses):
             state.appendleft(clause)
         reward = 0
+        print(f'reward is {reward}')
